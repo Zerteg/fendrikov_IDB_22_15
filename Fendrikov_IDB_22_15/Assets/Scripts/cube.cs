9a96ab2788c2b18862ee0653c3fd949c85ce0d79
Zerteg <fendrickov2017@yandex.ru>
2023-10-17 17:54:44 +0300

    task2

--- Fendrikov_IDB_22_15/Assets/Scripts/cube.cs
+++ Fendrikov_IDB_22_15/Assets/Scripts/cube.cs
@@ -5,11 +5,12 @@ using UnityEngine.UI;
 
 public class cube : MonoBehaviour
 {
- 
+    public float Speed = 10f;
+    public int count;
     public GameObject CUBE;
-   
-
-
+    public Text x;
+    public GameObject cam;
+    public GameObject bcam;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +21,62 @@ public class cube : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        if(Input.GetKey(KeyCode.W)) { 
+            transform.Translate(Vector3.forward* Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            transform.Rotate(Vector3.down * 5 * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            transform.Translate(Vector3.back * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            transform.Rotate(Vector3.up * 5 * Speed * Time.deltaTime);
+        }
+        if (count <= 0)
+        {
+           
+            this.gameObject.SetActive(false);
+            x.text = "Игра окончена";
+        }
+        else x.text = count.ToString(); 
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("CEN");
+        this.gameObject.GetComponent<Renderer>().material.color = Color.red;
+        count--;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("CET");
+        this.gameObject.GetComponent<Renderer>().material.color = Color.white;
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("CS");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("TE");
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("TX");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("TS");
+    }
+
+    public void heal()
+    {
+        count = 5;
     }
 
     public void bebra()
